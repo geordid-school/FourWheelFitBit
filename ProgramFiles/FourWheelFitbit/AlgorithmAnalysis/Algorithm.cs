0a57bac0e0fbbbd2f9a969d4cc1b039c2d6efd7c
Jacob Theobald <jcbtheo@gmail.com>
2018-11-13 23:19:08 -0600

    Add sequence diagrams and parts of section 4

--- ProgramFiles/FourWheelFitbit/AlgorithmAnalysis/Algorithm.cs
+++ ProgramFiles/FourWheelFitbit/AlgorithmAnalysis/Algorithm.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Data;
 namespace FourWheelFitbit.Algorithm
 {
     public class Algorithm
@@ -10,7 +10,7 @@ namespace FourWheelFitbit.Algorithm
         //Jon's code goes here
         //for now it just returns bunk data so we can test the api
 
-        public string AnalyzeWheelchairData(string data)
+        public string AnalyzeWheelchairData(DataTable wheelchairData)
         {
             return "formatted data return would be here.";
         }
